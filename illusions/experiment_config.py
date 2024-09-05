10e8b51bd107e37e4e7c5114ed27605440334581
emrecncelik <emrecncelik@gmail.com>
2024-09-05 13:36:50 +0300

    add some other models

--- illusions/experiment_config.py
+++ illusions/experiment_config.py
@@ -2,7 +2,7 @@ import os
 
 PROJECT_DIR = os.path.abspath(os.curdir)
 STIMULI = {
-    "warren1961": [
+    "warren1968": [
         "tress",
         "lame duck",
         "see",
@@ -18,5 +18,13 @@ STIMULI = {
 }
 
 MODELS = {
-    "CTC": [],
+    "wav2vec2": [
+        "facebook/wav2vec2-base-100h",
+        "facebook/wav2vec2-base-960h",
+        "facebook/wav2vec2-large-960h",
+        "facebook/wav2vec2-large-960h-lv60-self",
+    ],
+    "wav2vec2bert": ["hf-audio/wav2vec2-bert-CV16-en"],
+    "speecht5": ["microsoft/speecht5_asr"],
+    "whisper": ["openai/whisper-tiny.en"],
 }
